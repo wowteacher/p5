314c96a956c25ce84a89e785ac6ae76e87c11c79
Francesca <wowteacher@users.noreply.github.com>
2016-10-14 15:37:08 +0200

    Update sketch.js
    dopo aver usato git clone (push) provo a sincronizzare la copia sul computer

--- codingChallenge38/sketch.js
+++ codingChallenge38/sketch.js
@@ -6,6 +6,7 @@
 // Francesca 14 Sept 2016
 //Coding challenge #38 word interactor
 // ciao io sono clonato
+// dopo aver usato git clone (push) provo a sincronizzare la copia sul computer
 
 var textfield;
 var output;
